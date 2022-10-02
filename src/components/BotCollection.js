1a371857a00817c96345680aeced0b82826ca839
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-02 23:53:53 +0300

    Setting up bot mobility

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,11 +1,23 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function BotCollection() {
-  // Your code here
+function BotCollection({addABot, botData, dischargeBot}) {
+  // Your code here //transferred to App.js
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
+        {/*mapping through the bots to return a single bot-card for each bot*/
+        botData.map(bot => {
+          return (
+            <BotCard 
+              key={bot.id}
+              bot={bot} 
+              handleBots={addABot}
+              dischargeBot={dischargeBot}
+            />
+          )
+        })
+        }
         Collection of all bots
       </div>
     </div>
